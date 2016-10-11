fcc0dcb14b4a9c050bffc79c80d5d5b072afee1c
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-10-11 10:19:47 -0700

    Refactored from previous tab_reveal to make reveals and routes.js operate as Controllers

--- scripts/views/jobPostView.js
+++ scripts/views/jobPostView.js
@@ -1,32 +1,3 @@
 (function(module) {
-  searchView = {};
-  resultsView = {};
-  companyView = {};
-  favoritesView = {};
-
-  searchView.reveal = function() {
-    $('.tab-content').hide();
-    $('#landing-page').fadeIn('slow');
-  }
-
-  resultsView.reveal = function() {
-    $('.tab-content').hide();
-    $('#results-page').fadeIn('slow');
-  }
-
-  companyView.reveal = function() {
-    $('.tab-content').hide();
-    $('#company-page').fadeIn('slow');
-  }
-
-  favoritesView.reveal = function() {
-    $('.tab-content').hide();
-    $('#favorites-page').fadeIn('slow');
-  }
-
-  module.searchView = searchView;
-  module.resultsView = resultsView;
-  module.companyView = companyView;
-  module.favoritesView = favoritesView;
 
 })(window);
