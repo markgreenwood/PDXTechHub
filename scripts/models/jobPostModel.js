b7b014bf92abe398eab7f383057abb1a7f9455c2
mizutombo <thomas.shultz52@gmail.com>
2016-10-11 14:35:20 -0700

    revd code to enable data to render to html locations ... debug in progress

--- scripts/models/jobPostModel.js
+++ scripts/models/jobPostModel.js
@@ -11,16 +11,18 @@
   JobPost.prototype.toHtml = function() {
     var $newJob = $('article.template').clone();
     $newJob.removeAttr('class');
-    $newJob.find('h2').text('this.jobtitle');
+    console.log($newJob);
+    $newJob.find('h2').append('this.jobtitle');
     return $newJob;
   };
 
-  JobPost.fetchResults = function() {
+  JobPost.fetchResults = function(nextFunction) {
     //TODO: Eventually route this through server.js to avoid CORS issues
     var url = 'http://cors.io/?http://api.indeed.com/ads/apisearch?publisher=7094754948491444&q=javascript&l=portland,or&sort=&radius=&st=&jt=&start=&limit=50&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json';
-    $.getJSON(url + '', function(data) {
+    $.getJSON(url, function(data) {
       console.log(data);
       JobPost.loadAll(data.results);
+      nextFunction();
     });
   };
 
