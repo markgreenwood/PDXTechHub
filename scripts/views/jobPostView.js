26f301a3b3449f6a8c236c9710c2810f9b0bef7d
dratech <alfonsotech@nym.hush.com>
2016-10-11 16:35:39 -0700

    added Handlebars script template and script tag; added Handlebars compiler method; enabled API data to render to results page

--- scripts/views/jobPostView.js
+++ scripts/views/jobPostView.js
@@ -3,10 +3,9 @@
   var jobView = {};
 
   jobView.renderJobResults = function(){
-    console.log('running render job results on ' + JobPost.allJobPosts.length + 'objects');
     JobPost.allJobPosts.forEach(function(job){
-      console.log('inside forEach loop');
-      $('#results-page').append(job.toHtml() );
+      console.log('inside forEach loop', job);
+      $('#results-page').append( job.toHtml() );
       //JobPost.prototype.toHtml
     });
   };
