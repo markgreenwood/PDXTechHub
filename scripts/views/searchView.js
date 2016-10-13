7dd9dea1a5704c4af05a0be6ab86daccd98f5a6d
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-10-12 17:15:35 -0700

    Changed selector from biz type to language, got search working again

--- scripts/views/searchView.js
+++ scripts/views/searchView.js
@@ -5,9 +5,9 @@
     $('#submit-search').on('click', function() {
       var city = $('#city-selector').val();
       var radius = $('#radius-selector').val();
-      var biztype = $('#biz-type-selector').val();
-      console.log('City: ' + city + ', Radius: ' + radius + ', Biztype: ' + biztype);
-      resultsView.getResults({ 'city': city, 'radius': radius, 'biztype': biztype });
+      var lang = $('#language-selector').val();
+      console.log('City: ' + city + ', Radius: ' + radius + ', Language: ' + lang);
+      resultsView.getResults({ 'city': city, 'radius': radius, 'language': lang });
     });
   };
 
