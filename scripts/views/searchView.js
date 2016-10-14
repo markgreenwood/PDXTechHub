883129a74735bb0e299e16683dc2f2c0c5c056cc
Mark Greenwood <mrkjgreenwood@gmail.com>
2016-10-14 10:53:40 -0700

    Added comments to JS, stripped some console.logs

--- scripts/views/searchView.js
+++ scripts/views/searchView.js
@@ -1,17 +1,20 @@
 (function(module) {
   searchView = {};
 
+  // Click listener for the Submit button which fires off the search, and then
+  // redirects the user to the results page
   searchView.handleSubmitButton = function() {
     $('#submit-search').on('click', function() {
       var city = $('#city-selector').val();
       var radius = $('#radius-selector').val();
       var lang = $('#language-selector').val();
-      console.log('City: ' + city + ', Radius: ' + radius + ', Language: ' + lang);
+      //console.log('City: ' + city + ', Radius: ' + radius + ', Language: ' + lang);
       resultsView.getResults({ 'city': city, 'radius': radius, 'language': lang });
       resultsController.reveal();
     });
   };
 
+  // Register the click listener
   searchView.handleSubmitButton();
 
   module.searchView = searchView;
